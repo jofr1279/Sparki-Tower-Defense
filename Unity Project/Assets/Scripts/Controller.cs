ff4eb79261f14b6946a145d15c83aa3933a901df
Joshua Franklin <joshuafranklin26@gmail.com>
2019-11-26 12:46:13 -0700

    Add basic tower placing mechanics

---
+++ Unity Project/Assets/Scripts/Controller.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour {
+    public Camera arCamera;
+    public GameObject tower;
+    public Transform scaler;
+    public Text moneyText;
+    
+    public int money;
+    public int towerCost;
+
+    bool _fingerDown = false;
+
+    private void Start() {
+        UpdateMoneyText();
+    }
+
+    void Update() {
+        if (Input.touchCount >= 1) {
+            if (_fingerDown) return;
+            _fingerDown = true;
+            
+            if (money >= towerCost) {
+                Debug.Log("Building...");
+                BuildTurret(Input.touches[0].position);
+                money -= towerCost;
+                UpdateMoneyText();
+            }
+            else {
+                Debug.Log("Insufficient funds.");
+            }
+        }
+        else {
+            _fingerDown = false;
+        }
+    }
+
+    void BuildTurret(Vector2 position) {
+        Ray ray = arCamera.ScreenPointToRay(Input.GetTouch(0).position);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit)) {
+            if (hit.transform.CompareTag("Raycast Blanket")) {
+                Instantiate(tower, hit.point, scaler.rotation, scaler);
+            }
+        }
+        Debug.Log(position);
+    }
+
+    void UpdateMoneyText() {
+        moneyText.text = "Money: $" + money.ToString();
+    }
+}
---
+++ Unity Project/Assets/Scripts/TowerController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+public class TowerController : MonoBehaviour {
+    private Transform target;
+    public Transform turret;
+
+    public ParticleSystem fireParticles;
+    AudioSource _audioSource;
+    
+    public int range;
+    public float maxCooldown;
+    float _cooldown;
+
+    void Start() {
+        _audioSource = GetComponent<AudioSource>();
+        target = GameObject.FindWithTag("Sparki").transform;
+        _cooldown = maxCooldown;
+    }
+
+    void Update() {
+        if (Vector3.Distance(transform.position, target.position) <= range) {
+            // Hacky way to get the turret to only rotate on its Y axis
+            turret.LookAt(target);
+            turret.localRotation = Quaternion.Euler(
+                0,
+                turret.localRotation.eulerAngles.y,
+                0
+            );
+
+            if (_cooldown <= 0) {
+                Fire();
+                _cooldown = maxCooldown;
+            }
+        }
+
+        if (_cooldown > 0) {
+            _cooldown -= Time.deltaTime;
+        }
+    }
+
+    void Fire() {
+        fireParticles.Play();
+        _audioSource.Play();
+    }
+}
