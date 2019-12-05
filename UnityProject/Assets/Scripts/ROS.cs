1560cb12c46ddb3fb1354f93d63e7754b8aa7aa4
Joshua Franklin <joshuafranklin26@gmail.com>
2019-12-04 21:55:58 -0700

    Add update servo function

--- UnityProject/Assets/Scripts/ROS.cs
+++ UnityProject/Assets/Scripts/ROS.cs
@@ -1,5 +1,6 @@
 ﻿using RosSharp.RosBridgeClient;
 using RosSharp.RosBridgeClient.MessageTypes.Geometry;
+using RosSharp.RosBridgeClient.MessageTypes.Std;
 using RosSharp.RosBridgeClient.Protocols;
 
 using UnityEngine;
@@ -14,9 +15,14 @@ public class ROS {
 
     void InitTopics() {
         socket.Subscribe<Pose2D>("/sparki/odometry", UpdateOdometry);
+        socket.Subscribe<Int16>("/sparki/set_servo", UpdateServo);
     }
 
     static void UpdateOdometry(Pose2D pose) {
         Debug.Log($"Odometry update received! ({pose.x}, {pose.y}, {pose.theta})");
+    }
+    
+    static void UpdateServo(Int16 angle) {
+        Debug.Log($"Servo update received! ({angle.data})");
     }
 }
